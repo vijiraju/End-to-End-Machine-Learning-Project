77d14951c25fc2bd50aa4ac82a2d6d92f008ba17
vijiraju <vijiraju1947@gmail.com>
2024-01-15 21:32:55 +0530

    Data from github converetd to zip

--- src/mlProject/utils/common.py
+++ src/mlProject/utils/common.py
@@ -1,13 +1,14 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from mlProject import logger
+from src.mlProject import logger
 import json
 import joblib
 from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
+
 
 
 
